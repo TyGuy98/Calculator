5912d91da3c5d2f06f6da93db9978664a738c553
TyGuy98 <LittleCharon@protonmail.com>
2024-11-19 21:07:56 +0000

    Add basic operator functions

---
+++ script.js
@@ -0,0 +1,36 @@
+function addNumbers (num1, num2){
+    return num1 + num2;
+}
+
+function subtractNumbers (num1, num2){
+    return num1 - num2;
+}
+
+function divideNumbers (num1, num2){
+    return num1 / num2;
+}
+
+function multiplyNumbers (num1, num2){
+    return num1 * num2;
+}
+
+function operate (num1, operator, num2){
+    if (operator === "+"){
+        return addNumbers(num1, num2);
+    }
+
+    if (operator === "-"){
+        return subtractNumbersNumbers(num1, num2);
+    }
+
+    if (operator === "/"){
+        return divideNumbersNumbers(num1, num2);
+    }
+
+    if (operator === "*"){
+        return multiplyNumbers(num1, num2);
+    }
+}
+
+
+console.log(operate(5, "*", 5));
